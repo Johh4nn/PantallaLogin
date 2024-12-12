7a556336ec134aa1fc0233664f7238eea02298ab
Johan <johanvargas1212@gmail.com>
2024-12-11 22:57:00 -0500

    Initial commit

---
+++ src/main/java/org/example/form1.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class form1 {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton ingresarButton;
+    public JPanel mainPanel;
+    private JLabel validar;
+
+    public form1() {
+        ingresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String usuario = textField1.getText();
+                String passw = textField2.getText();
+                if(usuario.equals("Vargas") && passw.equals("Vargas123")){
+                        validar.setText("INGRESO EXITOSO");
+                }
+                else {
+                    validar.setText("ACCESO DENEGADO");
+                }
+            }
+        });
+    }
+}
